4c97d9b830f67fa2dd8cb93bf1643ff64a30a302
Indranil Tiwary <indranil-tiwary@users.noreply.github.com>
2018-08-28 07:34:49 +0530

    Initial implementation of rest api

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+//Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+//Author Struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+//Init books var as a slice Book struct
+var books []Book
+
+// Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Create a New Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	//Init Router
+	r := mux.NewRouter()
+
+	// Mock Data - TODO - implement DB
+	books = append(books, Book{ID: "1", Isbn: "123456", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "223456", Title: "Book Two", Author: &Author{Firstname: "Neil", Lastname: "Tiwary"}})
+	//Route Handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
